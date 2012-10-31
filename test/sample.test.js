2077eb8a3d75189c05e85b3eac2a4310fa84acdc
Carlos Avila <carlosavila.ce@gmail.com>
2012-10-31 13:48:29 -0700

    reduced sample.test.js to mocha syntax only by removing util functionality

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,11 +1,10 @@
 var should = require('chai').should()
 	, SampleController = require('../controllers/sample.controller.js')
-	, util = require('util')
 	, sampleMockDB = require('./sample.mockdb.js');
 
 var sampleController = null;
 
-suite('sample.controller.js', function (){
+describe('sample.controller.js', function() {
 
 	setup(function (done) {
 		sampleController = new SampleController(sampleMockDB, null, null, null);
@@ -19,18 +18,14 @@ suite('sample.controller.js', function (){
 		done();
 	});
 
-	suite('list()', function() {
-
-		suite(util.format(
-			'Given no parameters'), function () {
-			test('should return array of all samples', function (done){
-				sampleController.list(function (err, samples){
-					should.not.exist(err);
-					should.exist(samples);
-					samples.should.not.be.empty;
-					samples.should.be.an.instanceOf(Array);
-					done();
-				});
+	describe('#list()', function () {
+		it('should return array of all samples given no filters', function (done) {
+			sampleController.list(function (err, samples){
+				should.not.exist(err);
+				should.exist(samples);
+				samples.should.not.be.empty;
+				samples.should.be.an.instanceOf(Array);
+				done();
 			});
 		});
 	});
